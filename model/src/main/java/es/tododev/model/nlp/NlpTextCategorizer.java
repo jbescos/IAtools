0ff2eba2ec8232dcc7e950001096cdd9d3b5854c
jbescos <jorgebescos@gmail.com>
2018-04-01 10:01:17 +0200

    Some performance in tests

--- model/src/main/java/es/tododev/model/nlp/NlpTextCategorizer.java
+++ model/src/main/java/es/tododev/model/nlp/NlpTextCategorizer.java
@@ -2,6 +2,7 @@ package es.tododev.model.nlp;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
@@ -22,38 +23,44 @@ import opennlp.tools.util.MarkableFileInputStreamFactory;
 import opennlp.tools.util.ObjectStream;
 import opennlp.tools.util.PlainTextByLineStream;
 import opennlp.tools.util.TrainingParameters;
+import opennlp.tools.util.model.ModelType;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class NlpTextCategorizer {
 
 	private final static Logger log = LogManager.getLogger();
+	private final DocumentCategorizerME myCategorizer;
 	
-	public Entry<Double, String> categorize(File readFileModel, String ... texts) throws IOException {
+	public NlpTextCategorizer(File readFileModel) throws FileNotFoundException, IOException {
 		try (InputStream modelIn = new FileInputStream(readFileModel)) {
 			DoccatModel model = new DoccatModel(modelIn);
-			DocumentCategorizerME myCategorizer = new DocumentCategorizerME(model);
-			SortedMap<Double, Set<String>> map = myCategorizer.sortedScoreMap(texts);
-			log.debug(map.toString());
-			double probability = map.lastKey();
-			String category = map.get(probability).iterator().next();
-			Map.Entry<Double, String> pair = new AbstractMap.SimpleImmutableEntry<>(probability, category);
-			return pair;
+			myCategorizer = new DocumentCategorizerME(model);
 		}
 	}
 	
-	public void trainCategorizer(File trainingFile, OutputStream modelOut) throws IOException {
+	public Entry<Double, String> categorize(String ... texts) {
+		SortedMap<Double, Set<String>> map = myCategorizer.sortedScoreMap(texts);
+		log.debug(map.toString());
+		double probability = map.lastKey();
+		String category = map.get(probability).iterator().next();
+		Map.Entry<Double, String> pair = new AbstractMap.SimpleImmutableEntry<>(probability, category);
+		return pair;
+	}
+	
+	public static void trainCategorizer(File trainingFile, OutputStream modelOut) throws IOException {
 		trainCategorizer(trainingFile, modelOut, "en");
 	}
 
-	public void trainCategorizer(File trainingFile, OutputStream modelOut, String language) throws IOException {
+	public static void trainCategorizer(File trainingFile, OutputStream modelOut, String language) throws IOException {
 		ObjectStream<String> lineStream = new PlainTextByLineStream(new MarkableFileInputStreamFactory(trainingFile), "UTF-8");
 		ObjectStream<DocumentSample> sampleStream = new DocumentSampleStream(lineStream);
 		TrainingParameters trainingParameters = new TrainingParameters();
-//	    trainingParameters.put(TrainingParameters.ALGORITHM_PARAM, ModelType.MAXENT.name());
-		trainingParameters.put(TrainingParameters.ALGORITHM_PARAM, NaiveBayesTrainer.NAIVE_BAYES_VALUE);
-	    trainingParameters.put(TrainingParameters.ITERATIONS_PARAM, "100");
-	    trainingParameters.put(TrainingParameters.CUTOFF_PARAM, "0");
+	    trainingParameters.put(TrainingParameters.ALGORITHM_PARAM, ModelType.MAXENT.name());
+//		trainingParameters.put(TrainingParameters.ALGORITHM_PARAM, NaiveBayesTrainer.NAIVE_BAYES_VALUE);
+//	    trainingParameters.put(TrainingParameters.ITERATIONS_PARAM, "50");
+//	    trainingParameters.put(TrainingParameters.CUTOFF_PARAM, "0");
 	    trainingParameters.put(TrainingParameters.THREADS_PARAM, "100");
 //		parameters.put(AbstractTrainer.CUTOFF_PARAM, "1");
 		DoccatFactory factory = new DoccatFactory();
@@ -61,7 +68,7 @@ public class NlpTextCategorizer {
 		model.serialize(modelOut);
 	}
 	
-	public void addInTraininFile(File trainDataFile, String category, String content) throws IOException {
+	public static void addInTraininFile(File trainDataFile, String category, String content) throws IOException {
 		try(FileWriter fw = new FileWriter(trainDataFile, true)){
 			fw.write(category+" "+content.replaceAll("\r", "").replaceAll("\n", " ")+"\n");
 		}
--- model/src/test/java/es/tododev/model/nlp/NlpTextCategorizerTest.java
+++ model/src/test/java/es/tododev/model/nlp/NlpTextCategorizerTest.java
@@ -7,7 +7,9 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URL;
 import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
@@ -17,28 +19,29 @@ import org.junit.Test;
 
 import es.tododev.model.nlp.NlpTextCategorizer;
 import es.tododev.model.utils.ZipUtils;
+import es.tododev.model.utils.ZipUtils.ZipFile;
 
 public class NlpTextCategorizerTest {
 	
 	private final static Logger log = LogManager.getLogger();
 	// [tech, politics, business, entertainment, sport]
 	private final static String TRAIN_DATA = "/data/bbc-fulltext.zip";
-	private final NlpTextCategorizer nlpTextCategorizer = new NlpTextCategorizer();
-	private final File model = Files.createTempFile("trainedModel", ".model").toFile();
-	private final Map<String, String> tests = new HashMap<>();
+	private final NlpTextCategorizer nlpTextCategorizer;
+	private final List<ZipFile> tests = new ArrayList<>();
 	
 	public NlpTextCategorizerTest() throws IOException {
+		File model = Files.createTempFile("trainedModel", ".model").toFile();
 		File trainData = Files.createTempFile("trainData", ".txt").toFile();
 		log.debug("Creating training file in {}", trainData.getAbsolutePath());
 		URL zipFileURL = getClass().getResource(TRAIN_DATA);
 		try(InputStream inputStream = zipFileURL.openStream()){
 			ZipUtils.walkInZip(inputStream, entry -> {
 				int rnd = getRandom(0, 9);
-				if(rnd % 3 == 0) {
-					tests.put(entry.getDirectory(), entry.getContent());
+				if(rnd == 1) {
+					tests.add(entry);
 				}else {
 					try {
-						nlpTextCategorizer.addInTraininFile(trainData, entry.getDirectory(), entry.getContent());
+						NlpTextCategorizer.addInTraininFile(trainData, entry.getDirectory(), entry.getContent());
 					} catch (IOException e) {
 						log.error("Cannotwrite in training file "+entry, e);
 					}
@@ -47,19 +50,20 @@ public class NlpTextCategorizerTest {
 			});
 		}
 		try(OutputStream modelOut = new FileOutputStream(model)){
-			nlpTextCategorizer.trainCategorizer(trainData, modelOut);
+			NlpTextCategorizer.trainCategorizer(trainData, modelOut);
 		}
-		
+		trainData.delete();
+		nlpTextCategorizer = new NlpTextCategorizer(model);
 	}
 	
 	@Test
 	public void modelTest() throws IOException {
 		int success = 0;
 		int total = 0;
-		for(Entry<String, String> test : tests.entrySet()) {
+		for(ZipFile test : tests) {
 			total++;
-			Entry<Double, String> entry = nlpTextCategorizer.categorize(model, new String[]{test.getValue()});
-			if(entry.getValue().equals(test.getKey())) {
+			Entry<Double, String> entry = nlpTextCategorizer.categorize(new String[]{test.getContent()});
+			if(entry.getValue().equals(test.getDirectory())) {
 				success++;
 			}
 		}
